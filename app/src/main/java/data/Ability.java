f7cce51659b252edf71d26ca39bd61cf88b3f354
sukebill <sdi0700294@di.uoa.gr>
2016-02-06 18:23:14 +0200

    created discipline information screen

---
+++ app/src/main/java/data/Ability.java
@@ -0,0 +1,60 @@
+package data;
+
+
+public class Ability {
+
+    private int id;
+    private String title;
+    private String path;
+    private String description;
+    private String system;
+    private int level;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSystem() {
+        return system;
+    }
+
+    public void setSystem(String system) {
+        this.system = system;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
--- app/src/main/java/data/Constants.java
+++ app/src/main/java/data/Constants.java
@@ -3,4 +3,5 @@ package data;
 
 public class Constants {
     public static final String EXTRA_CLAN = "vtm.clan";
+    public static final String EXTRA_DISCIPLINE = "vtm.discipline";
 }
